4805fa9fd403b3aa4874a25ce12b26e413f79efb
vereyleyes <vreyleyes@udesa.edu.ar>
2023-09-08 16:27:56 -0300

    header falta css

---
+++ proyectoiii/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+           <img src="" alt="Logo de la app" /> {/*Opción Logo*/}
+           <h1>Nombre de la app</h1> {/*Opción Nombre*/}
+           <nav>
+                <ul>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/favoritos"}>Favoritos</Link></li>
+                    <li><Link to={"/ver-todas-las-canciones"}>Ver todas las canciones</Link></li>
+                    <li><Link to={"/ver-todos-los-albumes"}>Ver todos los álbumes</Link></li>
+                </ul>
+           </nav>
+        </header>
+    );
+}
+
+export default Header;
