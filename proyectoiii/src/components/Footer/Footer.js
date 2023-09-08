faa8e83b1079aa78f438590c17241f6614e3b832
vereyleyes <vreyleyes@udesa.edu.ar>
2023-09-08 19:13:07 -0300

    footer y package

---
+++ proyectoiii/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            <p>Virginia</p>
+            <p>Belen</p>
+            <p>Josefina</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- proyectoiii/src/screens/Home/Home.js
+++ proyectoiii/src/screens/Home/Home.js
@@ -35,7 +35,7 @@ class Home extends Component {
             })
             .catch(error => console.log(error))
     }
-    
+
     render() {
         return (
             <>
