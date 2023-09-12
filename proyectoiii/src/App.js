9849661af8e76905fdd4b7a1cb6b670c217faae2
belendominguezda <mdominguezdealzaga@udesa.edu.ar>
2023-09-12 10:59:26 -0300

    agregue importaciones de app

--- proyectoiii/src/App.js
+++ proyectoiii/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './screens/Home/Home';
+import DetalleCancion from './screens/DetalleCancion/DetalleCancion';
+import Favoritos from './screens/Favoritos/Favoritos';
+import VerTodasLasCanciones from './screens/VerTodasLasCanciones/VerTodasLasCanciones';
+import ResultadoBusqueda from './components/ResultadosBusqueda/ResultadoBusqueda';
 
 import './styles.css'
 
@@ -9,10 +13,12 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact={true} component={Home} />
-        <Route path="/cancion/:id" component={""} />
-        <Route path="/album/:id" component={""} />
-        <Route path="/favoritos" component={""} />
-        <Route path="/resultado-busqueda" component={""} />
+        <Route path="/cancion/:id" component={DetalleCancion} /> {/*Detalle cancion*/}
+        <Route path="/album/:id" component={""} /> {/*Detalle album*/}
+        <Route path="/favoritos" component={Favoritos} />
+        <Route path="/ver-todas-las-canciones" component={VerTodasLasCanciones} /> {/*Ver todas las canciones*/}
+        <Route path="/ver-todos-los-albums" component={""} /> {/*Ver todos los albums*/}
+        <Route path="/resultado-busqueda/:busqueda" component={ResultadoBusqueda} />
       </Switch>
     </BrowserRouter>
   );
